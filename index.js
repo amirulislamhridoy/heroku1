34453bdb9a9090d25d0c0bf913af1e3fe7179fd6
Your Name <hossainhridoy@30687@gmail.com>
2022-05-30 17:44:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/o', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/oo', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/ooo', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
